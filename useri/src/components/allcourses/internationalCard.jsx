08430dc36c77f021a1b227c577ee73b9b0dbfc53
nsr-Sul <115785885+nsr-Sul@users.noreply.github.com>
2023-03-26 17:31:56 +0300

    fixed international programs page
    added styling to international programs page

--- useri/src/components/allcourses/internationalCard.jsx
+++ useri/src/components/allcourses/internationalCard.jsx
@@ -2,55 +2,58 @@ import React from "react";
 import "./International.scss";
 import useFetch from "../useFetch";
 import Grid from "@mui/material/Grid";
-
+import { Link } from "react-router-dom";
 function Internationalcard() {
   const {
     error,
     isPending,
-    data: online,
+    data: Programs,
   } = useFetch("http://localhost:8000/coursesCard");
 
   return (
     <div className="maincomunitycarddiv">
       {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {online &&
-        online.map((val) => (
-          <div className="fathercontainercard" key={val.id}>
+      {Programs &&
+        Programs.map((val) => (
+          <Link to={`../coursesCard/${val.id}`} style={{"textDecoration": "none"}}>
+ <div className="fathercontainercard" key={val.id}>
             <div className="headercomunity">
               <div className="cardourcomunityrowdisplay">
                 <div className="firstcontentcomunity">
                   <h4>{val.coursesName}</h4>
-                  <h4>{val.TARGET}</h4>
+                  <h4>Target: {val.TARGET}</h4>
                 </div>
                 <div className="secondcomuitydiv">
-                  {val.text?.split("\n").map((line, index) => (
-                    <React.Fragment key={index}>
-                      {line}
-                      <br />
-                    </React.Fragment>
-                  ))}
-                </div>
-                <div className="proportiesdecs">
-                  <div className="deletoptioncardcomunuty">
-                    <Grid item xs={8}></Grid>
-                  </div>
-                  <div className="editoptioncardcomunuty">
-                    <Grid item xs={8}></Grid>
+                  <div>
+                    {val.courTeacher.map((source) => (
+                      <>
+                        <img className="imgSc" src={source} />
+                      </>
+                    ))}
                   </div>
                 </div>
               </div>
             </div>
             <div className="nonamedisplay">
-              <div className="image-container">
-                {val.courTeacher.map((source) => (
-                  <>
-                    <img src={source} />
-                  </>
-                ))}
+              <div className="textInter">
+                <p style={{ marginLeft: "0%" }}>
+                  <strong>Objective:</strong> {val.OBJECTIVE}
+                </p>
+                <p style={{ marginLeft: "0%" }}>
+                  <strong>Sector:</strong> {val.SECTOR}
+                </p>
+                <p style={{ marginLeft: "0%" }}>
+                  <strong>Duration:</strong> {val.DURATION}
+                </p>
+                <p style={{ marginLeft: "0%" }}>
+                  <strong>Type:</strong> {val.TYPE}
+                </p>
+                <span>{val.text}</span>
               </div>
             </div>
           </div>
+          </Link>
         ))}
     </div>
   );
