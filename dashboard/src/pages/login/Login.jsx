9681adf385828f96fc5afb9f2b13dfcacdeab39b
nsr-Sul <115785885+nsr-Sul@users.noreply.github.com>
2023-04-13 17:28:24 +0300

    fixed minor issues

--- dashboard/src/pages/login/Login.jsx
+++ dashboard/src/pages/login/Login.jsx
@@ -8,11 +8,14 @@ import bcrypt from "bcryptjs";
 import { AuthContext } from "../../context/AuthContext";
 
 function Login() {
-  
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const { data: users, isPending, error } = useFetch("http://localhost:8000/users");
+  const {
+    data: users,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/users");
 
   const { login, isAuthenticated } = useContext(AuthContext);
 
@@ -26,7 +29,7 @@ function Login() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     if (username.trim() === "") {
       setErrorMessage("Please enter a username");
     } else if (password.trim() === "") {
@@ -35,24 +38,21 @@ function Login() {
       setErrorMessage("Loading user data, please wait...");
     } else {
       const user = users.find((user) => user.username === username);
-  
+
       if (!user) {
         setErrorMessage("Username not found");
       } else {
-
         const passwordMatch = await bcrypt.compare(password, user.password);
-  
+
         if (!passwordMatch) {
           setErrorMessage("Incorrect password");
         } else {
           setErrorMessage("");
           console.log("Successful login!");
           login(user);
-          
         }
       }
     }
-    
   };
 
   return (
@@ -97,11 +97,11 @@ function Login() {
                 />
               </div>
               <div className="login-button">
-                <button className="buttonL" disabled={isPending}>Login</button>
+                <button className="buttonL" disabled={isPending}>
+                  Login
+                </button>
               </div>
-              {errorMessage && (
-                <p className="error-message">{errorMessage}</p>
-              )}
+              {errorMessage && <p className="error-message">{errorMessage}</p>}
             </form>
           </div>
         </div>
